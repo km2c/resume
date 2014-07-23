aae4fe857e75b7e34ef7ed6f1f560ba56960af31
Kirk McCutcheon <kmccutcheon@resource.com>
2014-07-23 10:40:04 -0400

    add social icons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,13 +3,29 @@
 var km2c = {
     links: function(link){
         window.open(link, '_blank');
+    },
+    mailMe: function(){
+        var emailAddress = ['kirk', '@', 'km2c.com'].join(''),
+            emailElement = document.getElementById('email');
+        emailElement.innerHTML = emailAddress;
+        emailElement.setAttribute('href', 'mailto:' +  emailAddress);
+    },
+    callMe: function(){
+        var phoneNumber = ['614', '989','3924'].join('-'),
+            phoneElement = document.getElementById('phone');
+        phoneElement.innerHTML = phoneNumber;
+        phoneElement.setAttribute('href', 'tel:' +  phoneNumber);
     }
 };
 
-$(function(){
-    $('.projects').on('click', 'a', function(e){
-        e.preventDefault();
-        var link = $(this).data('link');
-        km2c.links(link);
-    });
+
+$('.projects').on('click', 'a', function(e){
+    e.preventDefault();
+    var link = $(this).data('link');
+    km2c.links(link);
+});
+
+window.addEventListener('load', function() {
+    km2c.mailMe();
+    km2c.callMe();
 });
